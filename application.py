54e2cb363ad5c5a6a10f5d45d43c76faee5baa77
anvesh4161 <anveshbunty01@gmail.com>
2025-04-28 14:32:39 +0530

    Drift Detection updated

--- application.py
+++ application.py
@@ -2,6 +2,13 @@ import pickle
 import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
+from src.logger import get_logger
+
+from alibi_detect.cd import KSDrift
+from src.feature_store import RedisFeatureStore
+from sklearn. preprocessing import StandardScaler
+
+logger = get_logger(__name__)
 
 app = Flask(__name__, template_folder='templates')
 
@@ -11,6 +18,24 @@ with open(MODEL_PATH, 'rb') as model_file:
 
 FEATURE_NAMES = ['Age', 'Fare', 'Pclass', 'Sex', 'Embarked', 'Familysize', 'Isalone',
        'HasCabin', 'Title', 'Pclass_Fare', 'Age_Fare']
+
+feature_store = RedisFeatureStore()
+scaler = StandardScaler()
+
+def fit_scaler_on_ref_data():
+    entity_ids = feature_store.get_all_entity_ids()
+    all_features = feature_store.get_batch_features(entity_ids)
+
+    all_features_df = pd.DataFrame.from_dict(all_features, orient='index')[FEATURE_NAMES]
+
+    scaler.fit(all_features_df)
+
+    return scaler.transform(all_features_df)
+
+historical_data = fit_scaler_on_ref_data()
+ksd = KSDrift(x_ref=historical_data, p_val=0.05)
+
+
 
 @app.route('/')
 def home():
@@ -33,6 +58,20 @@ def predict():
         Age_Fare = float(data["Age_Fare"])
 
         features = pd.DataFrame([[Age, Fare, Pclass, Sex, Embarked, Familysize,Isalone, HasCabin, Title, Pclass_Fare, Age_Fare]], columns=FEATURE_NAMES)
+
+        ########################## Data Drift Detection###########################
+        features_scaled = scaler.transform(features)
+        drift = ksd.predict(features_scaled)
+
+        print("Drift Response:", drift)
+
+        drift_response = drift.get('data', {})
+        is_drift = drift_response.get('is_drift', None)
+
+        if is_drift is not None and is_drift == 1:
+            print("Data Drift Detected")
+            logger.info("Data Drift Detected")
+        
 
         prediction = model.predict(features)[0]
 
